04e5ccd26e2025c09b040ca98721f656dc639490
haru-NpzMKvTxsyoqWXtG5jAoP <cloven.placket.2l@icloud.com>
2025-06-03 02:06:57 +0900

    レイアウトデバッグのため、アイテム幅計算をcalcに戻す

--- js/custom-masonry.js
+++ js/custom-masonry.js
@@ -1,15 +1,16 @@
-import Masonry from './masonry.pkgd.min.js'; // Masonryをインポート
-import imagesLoaded from './imagesloaded.pkgd.min.js'; // imagesLoadedをインポート
-
 document.addEventListener('DOMContentLoaded', function () {
+  // メイソンリーレイアウトを適用するコンテナ要素を取得
   var elem = document.querySelector('.masonry-gallery');
   if (elem) {
+    // まず、elem（.masonry-gallery）の中の画像が全部読み込まれるのを待つ
     imagesLoaded( elem, function() {
+      // 画像が全部読み込まれたら、ここからMasonryの処理を始める！
       var msnry = new Masonry( elem, {
+        // オプション
         itemSelector: '.masonry-gallery-item',
-        columnWidth: 395,
-        gutter: 25,
-        percentPosition: false
+        columnWidth: '.masonry-gallery-item', // ← セレクタ指定に戻す
+        gutter: 25, // アイテム間の隙間
+        percentPosition: true // ← true に戻す
       });
     });
   }
