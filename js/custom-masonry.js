d3e8af29abed80b8948147c246e26a41c84d5e7f
haru-NpzMKvTxsyoqWXtG5jAoP <cloven.placket.2l@icloud.com>
2025-06-03 01:27:03 +0900

    Masonry初期化をsetTimeoutで遅延実行

--- js/custom-masonry.js
+++ js/custom-masonry.js
@@ -4,15 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
   if (elem) {
     // まず、elem（.masonry-gallery）の中の画像が全部読み込まれるのを待つ
     imagesLoaded( elem, function() {
-      // 画像が全部読み込まれたら、ここからMasonryの処理を始める！
-      var msnry = new Masonry( elem, {
-        // オプション
-        itemSelector: '.masonry-gallery-item',
-        columnWidth: 395, // ← 固定ピクセル値に変更
-        gutter: 25, // アイテム間の隙間
-        percentPosition: false // 幅が%指定なのでfalseに変更
-      });
-      msnry.layout(); // ← これを追加！
+      // imagesLoadedのコールバック全体をsetTimeoutで遅らせる
+      setTimeout(function() {
+        var msnry = new Masonry( elem, {
+          // オプション
+          itemSelector: '.masonry-gallery-item',
+          columnWidth: 395, // ← 固定ピクセル値に変更
+          gutter: 25, // アイテム間の隙間
+          percentPosition: false // 幅が%指定なのでfalseに変更
+        });
+        // msnry.layout(); // layout() はMasonryが自動で初回実行するので、一旦なくても良いかも
+      }, 100); // 100ミリ秒後に実行
     });
   }
 }); 
