45779dd47e4c609ddb6b52b88ad8df69628f27f6
haru-NpzMKvTxsyoqWXtG5jAoP <cloven.placket.2l@icloud.com>
2025-06-13 10:44:05 +0900

    feat: add adobe fonts script to functions.php

--- functions.php
+++ functions.php
@@ -65,3 +65,23 @@ function setup_custom_header() {
     add_action( 'wp_body_open', 'add_custom_vertical_header' );
 }
 add_action( 'init', 'setup_custom_header' );
+
+function add_adobe_fonts_script() {
+  // Adobe Fontsのスクリプト
+  $script = <<<EOD
+<script>
+  (function(d) {
+    var config = {
+      kitId: 'bew5wgt',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
+EOD;
+  
+  // headタグの最後にスクリプトを出力
+  echo $script;
+}
+add_action( 'wp_head', 'add_adobe_fonts_script' );
